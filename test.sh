7ad7d1f7b6e344a856d098aa2455b7e9bd6b4761
yassinenecib-eaton <yassinenecib@eaton.com>
2024-03-25 18:01:39 -0400

    Added strings to check the result when test fail

--- test.sh
+++ test.sh
@@ -2,7 +2,10 @@
 
 clear
 declare -i RESULT=0
+declare -i RESULT_FAIL=0
+declare -i CPT=0
 FAIL=""
+ALL_FAIL=1
 
 TEST_ARRAY=("machinectl | head -2" \
 )
@@ -11,49 +14,63 @@ TEST_APP=("journalctl -M pxmc | ")
 
 
 #test if pxmc container is alive
-ssh root@10.106.94.104 "machinectl | head -2"
 PXMC_ALIVE=$(ssh root@10.106.94.104 "machinectl | head -2")
 ANS=$(echo $PXMC_ALIVE | grep "pxmc container")
+#ANS=$(echo $PXMC_ALIVE | grep "toto container")
 if [ $? -eq 0 ]
 then
 	echo "pxmc container is alive ... ok :)"
 	RESULT=$((RESULT+1))
 #	echo "${RESULT}"
 else
-	FAIL="test if container is alive "
+	echo "pxmc container is NOT alive ... ok :("
+	FAIL+="test if container is alive "
+	RESULT_FAIL=$((RESULT_FAIL+1))
 fi
+CPT=$((CPT+1))
 echo ""
 
 #test if PXMCApplication is runing
 ssh root@10.106.94.104 "journalctl -M pxmc | grep -i \"PXMCApplication.*failed*\""
+#ssh root@10.106.94.104 "journalctl -M pxmc | grep -i -v \"PXMCApplication.*failed*\""
 if [ $? -eq 0 ]
 then
 	echo "PXMCApplication ERROR ... :("
+	FAIL+="PXMCApplication "
+	RESULT_FAIL=$((RESULT_FAIL+1))
 else
 	echo "PXMCApplication is alive ... ok :)"
 	RESULT=$((RESULT+1))
 #	echo "${RESULT}"
 fi	
+CPT=$((CPT+1))
 echo ""
 
 #test if postgresql is runing
 ssh root@10.106.94.104 "journalctl -M pxmc | grep -i \"postgresql.*failed*\""
+#ssh root@10.106.94.104 "journalctl -M pxmc | grep -i -v \"postgresql.*failed*\""
 if [ $? -eq 0 ]
 then
 	echo "postgresql ERROR ... :("
+	FAIL+="postgresql "
+	RESULT_FAIL=$((RESULT_FAIL+1))
 else
 	echo "postgresql is alive ... ok :)"
 	RESULT=$((RESULT+1))
 #	echo "${RESULT}"
 fi	
+CPT=$((CPT+1))
 echo ""
 
 #read cpu
 CPU=$(ssh root@10.106.94.104 "systemd-cgtop -M pxmc")
+#CPU=$(echo ${CPU} | awk '{print $9}')
 CPU=$(echo ${CPU} | awk '{print $3}')
-if [ $? -ne 0 ]
+if [ $? -ne 0 ] || [ -z "${CPU}" ]
 then
 	echo "ERROR to read CPU value"
+	FAIL+="CPU "
+	RESULT_FAIL=$((RESULT_FAIL+1))
 fi
 if [ "${CPU}" = "-" ]
 then
@@ -62,23 +79,29 @@ fi
 echo "CPU=${CPU}"
 RESULT=$((RESULT+1))
 #echo "${RESULT}"
-
+CPT=$((CPT+1))
 echo ""
+
 #read memory
 MEM=$(ssh root@10.106.94.104 "systemd-cgtop -M pxmc")
-if [ $? -ne 0 ]
+#MEM=$(ssh root@10.106.94.104 "systemd-cgtop -M pxi]wssmc")
+if [ $? -ne 0 ] || [ -z "${MEM}" ]
 then
 	echo "ERROR to read memory value"
+	FAIL+="memory "
+	RESULT_FAIL=$((RESULT_FAIL+1))
 fi
 #echo "MEM=${MEM}"
 MEM=$(echo ${MEM} | awk '{print $4}')
 echo "MEM=${MEM}"
 RESULT=$((RESULT+1))
 #echo "${RESULT}"
+CPT=$((CPT+1))
 echo ""
 
 #Test if UI is reachable
 wget -q https://10.106.94.104/pxmc --no-check-certificate
+#wget -q https://10.106.94.104/pxmc --no-checrtificate
 if [ $? -eq 0 ]
 then
 	echo "UI is reacheable ... ok :)"
@@ -86,11 +109,15 @@ then
 #	echo "${RESULT}"
 else
 	echo "UI is not reachable ERROR ... :("
+	FAIL+="UI "
+	RESULT_FAIL=$((RESULT_FAIL+1))
 fi
+CPT=$((CPT+1))
 echo ""
 
 #Test if syslog us runing in the container
 SYS=$(ssh root@10.106.94.104 "systemctl status systemd-nspawn@pxmc.service | grep syslog")
+#SYS=$(ssh root@10.106.94.104 "systemctl status systemd-nspawn@pxmc.service | grep toto")
 if [ $? -eq 0 ]
 then
 	echo "syslog is running ... ok :)"
@@ -98,14 +125,36 @@ then
 	#echo "${RESULT}"
 else
 	echo "Syslog is not running ERROR ... :("
+	FAIL+="syslog "
+	RESULT_FAIL=$((RESULT_FAIL+1))
 fi
 echo ""
-echo "##########################################################"
-if [ ${RESULT} -ne 7 ]
+CPT=$((CPT+1))
+#Test if we can read database
+SYS=$(ssh root@10.106.94.104 "ls /var/containers/data/pxmc")
+#SYS=$(ssh root@10.106.94.104 "ls /var/containers/data/pxmcdwcdwd")
+if [ $? -eq 0 ]
 then
-	RES=$(echo "scale=1; ${RESULT}/7" | bc -l)
-	RES=$(echo "scale=1; ${RES}*100" | bc -l)
-	echo "Test ${FAIL} failed, ${RES} % passed"
+	echo "Database ... ok :)"
+	RESULT=$((RESULT+1))
+	#echo "${RESULT}"
+else
+	echo "Database issue ERROR ... :("
+	FAIL+="Database "
+	RESULT_FAIL=$((RESULT_FAIL+1))
+fi
+CPT=$((CPT+1))
+echo ""
+
+
+echo "##########################################################"
+#echo "RESULT: ${RESULT}"
+#echo "CPT: ${CPT}"
+if [ ${RESULT} -ne ${CPT} ]
+then
+	RES=$(echo "scale=2; ${RESULT_FAIL}/${CPT}*100" | bc -l)
+	echo "The following test failed: ${FAIL}"
+	echo "${RES} % failed"
 else
 	echo "All tests passed :)"
 fi
