f8d599f9b1623ea7f229291f1c826bff214049a0
yassinenecib-eaton <yassinenecib@eaton.com>
2024-03-27 18:10:50 -0400

    GET request, regex to check ISO8601

---
+++ testApiTime.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+echo ##########GET request for time#############
+wget -S --no-check-certificate --header 'authorization: Basic YWRtaW46U2VjdXJpdHkuNHU=' -q  https://10.130.129.211/api/system/v1/time -O tmpfile
+clear
+
+#echo "$(cat tmpfile)"
+
+grep -o [0-9].*Z  tmpfile > tmpsfile2
+#echo "$(cat tmpsfile2)"
+ANS=$(grep -E -o [0-9]{4}-\(0[1-9]\|1[0-2]\)-\(0[1-9]\|1[0-9]\|2[0-9]\|3[0-1]\)T\(0[1-9]\|1[0-9]\|2[0-3]\):\([0-5][0-9]\):\([0-5][0-9]\).[0-9]{3}Z tmpsfile2)
+if [ $? -eq 0 ]
+then
+	echo "Correct UTC format: $ANS"
+fi
+rm tmp* file*
+echo ##########GET request for time#############
