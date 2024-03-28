200cf5f497e5189d9780f9351b43f584e88be8f8
yassinenecib-eaton <yassinenecib@eaton.com>
2024-03-28 14:45:06 -0400

    Added post request

--- testApiTime.sh
+++ testApiTime.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+IP=10.106.94.104
+TIME=2023-01-11T12:13:15Z
+USER=root
 clear
-echo ##########GET request for time#############
-wget --method GET --no-check-certificate -q --header 'authorization: Basic YWRtaW46U2VjdXJpdHkuNHU='  https://10.130.129.211/api/system/v1/time -O tmpfile
+echo "##########GET request for time#############"
+wget --method  GET --no-check-certificate -q --header 'authorization: Basic YWRtaW46U2VjdXJpdHkuNHU='  https://${IP}/api/system/v1/time -O tmpfile
 #echo "$(cat tmpfile)"
 
 grep -o [0-9].*Z  tmpfile > tmpsfile2
@@ -10,6 +13,31 @@ ANS=$(grep -E -o [0-9]{4}-\(0[1-9]\|1[0-2]\)-\(0[1-9]\|1[0-9]\|2[0-9]\|3[0-1]\)T
 if [ $? -eq 0 ]
 then
 	echo "Correct UTC format: $ANS"
+	printf "GET test \033[92m PASSED\n\033[0m"
+else
+	printf "GET test \033[91m FAILED\n\033[0m"
+	exit
 fi
-rm tmp* # file* 
-echo "##########GET request for time#############"
+echo "##########POST request for time#############"
+echo "This time: $TIME will be sent to the DA with this IP $IP"
+wget -q --no-check-certificate  --method POST --timeout=0 --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46U2VjdXJpdHkuNHU=' --body-data '{"Value":"'"${TIME}"'"}' https://$IP/api/system/v1/time
+
+if [ $? -ne 0 ]
+then
+	printf "wget command \033[91m FAILEd\n\033[0m"
+	exit
+else
+	printf "wget command \033[92m PASSED\n\033[0m"
+fi
+
+####Check result####
+ANS=$(ssh ${USER}@${IP} "systemctl status smp-fcgi-time | grep -E -o [0-9]{4}-\(0[1-9]\|1[0-2]\).* | tail -1")
+echo "$ANS"
+
+if [ ${ANS} = ${TIME} ]
+then
+	printf "POST test passed \033[92m PASSED\n\033[0m"
+else
+	printf "POST test passed \033[91m FAILED\n\033[0m"
+fi
+rm tmp* time* # file* 
